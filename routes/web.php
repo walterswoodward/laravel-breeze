9a1a92f0f9f5aa099d2918a7a523a86d636c4cdb
walterswoodward <walter.sekope.woodward@gmail.com>
2021-02-10 15:48:08 -0800

    Adds back welcome vat '/welcome' just for future ref

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/welcome', function () {	
+    return view('welcome');	
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
